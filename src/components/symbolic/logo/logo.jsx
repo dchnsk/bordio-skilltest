81af9598ef8d489301da467bd77e13a559dfda0b
dchnsk <leriidchnsk@gmail.com>
2022-11-26 20:43:18 +0300

    Change project logo svg file location

--- src/components/symbolic/logo/logo.jsx
+++ src/components/symbolic/logo/logo.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { ReactComponent as LogoSvg } from './bordio-logo.svg';
+import { ReactComponent as LogoSvg } from '../../../assets/icons/bordio-logo-ic.svg';
 
 const LogoStyled = styled.div`
     margin-bottom: 26px;
